9dff5eb5912bbc810057dd72b7ee5146277c6209
rachelha1211 <157726284+rachelha1211@users.noreply.github.com>
2024-04-02 10:19:29 -0400

    class category docstrings

--- financial_tracker.py
+++ financial_tracker.py
@@ -1,1 +1,16 @@
 """A personal financial tracker that will display total budget over the week"""
+
+class Category():
+    """A class for defining the name of a category
+    
+    Attributes:
+        name (str): name of the category
+    """
+    def __init__(self, name):
+        """Initializes category_name object
+        
+        Args: 
+            name (str): see class documentation
+        """
+        self.category_name = name
+        
