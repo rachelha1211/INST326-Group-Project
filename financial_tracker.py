02de39c78c4b2de59ef99ffdbf20f7c8685e31b1
yosun-cloud <121991788+yosun-cloud@users.noreply.github.com>
2024-04-06 14:31:15 -0400

    Update financial_tracker.py

--- financial_tracker.py
+++ financial_tracker.py
@@ -15,7 +15,23 @@ class Category():
         self.category_name = name
 
 expenses = []
-        
+
+expense1 = {'amount': '51.00', 'category': 'shirt'}
+expenses.append(expense1)
+expense2 = {'amount': '30', 'category': 'groceries'}
+expenses.append(expense2)
+
+
+def removeExpense():
+    while True:
+        listExpenses()
+        print("What expense would you like to remove?")
+        try:
+            expenseToRemove = int(input(">"))
+            del expenses[expenseToRemove]
+        except:
+            print("Invalid input. Please try again.")
+       
 def addExpense(amount, category):
     expense = {'amount': amount, 'category': category}
     expenses.append(expense)
@@ -32,3 +48,35 @@ def listExpenses():
     counter = 0 
     for expense in expenses:
         print("#", counter, "-", expense['amount'], "-", expense['category'])
+        counter += 1
+    print("\n\n")
+
+if __name__ == "__main__":
+    while True:
+        ###Prompt the user
+        printMenu()
+        optionSelected = input(">")
+        
+        if (optionSelected == "1"):
+            print("How much was this expense?")
+            while True:
+                try:
+                    amountToAdd = input("> ")
+                    break
+                except:
+                     print("Invalid input. Please try again.")
+            
+            print("What category was this expense?")
+            while True:
+                try:
+                    category = input("> ")
+                    break
+                except:
+                     print("Invalid input. Please try again.")
+            addExpense(amountToAdd, category)
+        elif(optionSelected == '2'):
+            removeExpense()
+        elif(optionSelected == '3'):
+            listExpenses()
+        else:
+             print("Invalid input. Please try again.")
