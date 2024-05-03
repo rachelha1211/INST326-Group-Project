7b9b60b9cc348a653067a645a991cf14ff91e798
rachelha1211 <157726284+rachelha1211@users.noreply.github.com>
2024-05-03 18:04:58 -0400

    some unit tests

--- test_financial_tracker.py
+++ test_financial_tracker.py
@@ -1,28 +1,26 @@
 """Test units for financial tracker."""
-import financial_tracker
-import unittest
-
-tracker = financial_tracker
+import finance_trackerfinal as ft
 
 
-class Test(unittest.TestCase):
-    def test_add(self):
-        """Test adding as if the item added was a $100 
-        food expense in the food category"""
-        tracker.addExpense(100, "Food")
-        temp = ""
-        temp += "Here is a list of your expenses\n"
-        temp += "--------------------------------\n"
-        temp += "# 0 - $ 51.00 - shirt\n\n"
-        temp += "# 1 - $ 30 - groceries\n\n"
-        temp += "# 2 - $100 - Food"
-        self.assertEqual(temp, tracker.listExpenses())
+class Test_create():
+    #fail
+    def test_expense(self):
+        expense = ft.Create_expenses()
+        expense.add_expense("Monday", 23, "Food")
+        exlist = expense.get_expense_list(self)
+        assert exlist == [ft.Expense("Monday", 23, "Food")]
 
-    def test_remove(self):
-        """Remove the food expense created in the method above"""
-        temp = ""
-        temp += "Here is a list of your expenses\n"
-        temp += "--------------------------------\n"
-        temp += "# 0 - $ 51.00 - shirt\n\n"
-        temp += "# 1 - $ 30 - groceries\n\n"
-        self.assertEqual(temp, tracker.listExpenses())
+class Test_Results():
+    #fail
+    def test_organize_days(self):
+        sorted_list = ft.Results.organize_days([ft.Expense("Wednesday", 10, "School"), ft.Expense("Monday", 23, "Food")])
+        assert sorted_list == [ft.Expense("Monday", 23, "Food"), ft.Expense("Wednesday", 10, "School")]
+    
+    #passed    
+    def test_calculate_total_week(self):
+        total = ft.Results.calculate_total_week([ft.Expense("Monday", 23, "Food"), ft.Expense("Wednesday", 10, "School")])
+        assert total == 33
+        
+class Test_Display():
+    def test_list():
+        ft.Display.listExpenses([ft.Expense("Monday", 23, "Food"), ft.Expense("Wednesday", 10, "School")])
